b50ae61df4c4f105f0cf7a00edda4a20ae7cac1c
vladimarus <cyberstalker@ya.ru>
2021-08-05 17:13:38 +0300

    5.6. Простые числа [#156314]

--- src/ru/job4j/loop/CheckPrimeNumber.java
+++ src/ru/job4j/loop/CheckPrimeNumber.java
@@ -6,7 +6,7 @@ public class CheckPrimeNumber {
 //        3. Метод должен вернуть true, если переменная number является простым числом.
         boolean prime = number != 1;
 
-        for (int i = 2; i < number; i++) {
+        for (int i = 2; i < number; i++) { // i <= Math.ceil(Math.sqrt(number)) // оптимизация границы поиска
             if (number % i == 0) {
                 prime = false;
                 break;
