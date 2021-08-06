3b0d4d7bd0fdf4b02397597cc313ea853c6f558a
vladimarus <cyberstalker@ya.ru>
2021-08-06 11:25:43 +0300

    6.0.1. Объявление массива. [#3613]

---
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -0,0 +1,12 @@
+package ru.job4j.array;
+
+public class ArrayDefinition {
+    public static void main(String[] args) {
+//        - Массив на 10 элементов типа short с именем ages.
+        short[] ages = new short[10];
+//        - Массив на 100500 элементов типа String c именем surnames.
+        String[] surnames = new String[100500];
+//        - Массив на 40 элементов типа float с именем prices.
+        float[] prices = new float[40];
+    }
+}
