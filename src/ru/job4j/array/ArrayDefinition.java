deb2d5d73774d042639395eb16a84f1ab4d5690a
vladimarus <cyberstalker@ya.ru>
2021-08-06 11:29:02 +0300

    6.0.1.1. Размер массива [#333582]

--- src/ru/job4j/array/ArrayDefinition.java
+++ src/ru/job4j/array/ArrayDefinition.java
@@ -4,9 +4,12 @@ public class ArrayDefinition {
     public static void main(String[] args) {
 //        - Массив на 10 элементов типа short с именем ages.
         short[] ages = new short[10];
+        System.out.println("Размер массива равен: " + ages.length);
 //        - Массив на 100500 элементов типа String c именем surnames.
         String[] surnames = new String[100500];
+        System.out.println("Размер массива равен: " + surnames.length);
 //        - Массив на 40 элементов типа float с именем prices.
         float[] prices = new float[40];
+        System.out.println("Размер массива равен: " + prices.length);
     }
 }
