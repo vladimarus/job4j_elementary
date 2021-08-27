fd40862bf178edff29c590c4e31b3f748280a7d9
vladimarus <cyberstalker@ya.ru>
2021-08-27 13:34:07 +0300

    CinemaTest исправление.

--- test/ru/job4j/array/CinemaTest.java
+++ test/ru/job4j/array/CinemaTest.java
@@ -2,7 +2,7 @@ package ru.job4j.array;
 
 import org.junit.Test;
 
-import static org.hamcrest.Matchers.nullValue;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.*;
 
